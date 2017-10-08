a75e01bcfa7f38f7aa294c6bfbc980c99bb25afc
Alex Tait <alexandratait@gmail.com>
2017-10-08 11:42:39 -0400

    adding text to link input

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -120,7 +120,7 @@ class App extends React.Component {
        						<form onSubmit={this.handleSubmit}>
        							<input type='text' name='userName' placeholder='your name' value={this.state.user.displayName || this.state.user.email} />
        							<input type='text' name='currentItem' placeholder="describe your resource" onChange={this.handleChange} value={this.state.currentItem} />
-       							<input type='text' name='link' placeholder='paste link here' onChange={this.handleChange} value={this.state.link} />
+       							<input type='text' name='link' placeholder='paste link here (include http)' onChange={this.handleChange} value={this.state.link} />
        							<button>Add Item</button>
        						</form>
        					</section>  
